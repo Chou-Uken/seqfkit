97638b4fb62323208b19b6842539059227969dc9
Chou_Uken <Chouuken@outlook.com>
2024-12-16 12:08:16 +0800

    try

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 Author: Chou_Uken Chouuken@outlook.com
 Date: 2024-12-13 14:33:47
 LastEditors: Chou_Uken Chouuken@outlook.com
-LastEditTime: 2024-12-14 18:12:52
+LastEditTime: 2024-12-14 18:20:43
 FilePath: /seqfkit/tests/test_utils.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -84,3 +84,5 @@ def test_complement_invalid_sequence():
         complement('1234')
     with pytest.raises(ValueError):
         complement('ATCGX')
+
+
